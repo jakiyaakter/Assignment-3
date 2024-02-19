5d4df447232101b6466edf38d1607b23feae09b9
jakiyaakter <jakkiyaakter744@gmai.com>
2024-02-19 14:48:04 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Array object:
+let mobile = ["Vivo", "Oppo", "Realme"];
+
+console.log(mobile);
+
+// Object:
+let person = {firstName:"Tisha", lastName:"Khan"};
+
+console.log(person);
+
+// push:
+let My name =['jakiya'];
+
